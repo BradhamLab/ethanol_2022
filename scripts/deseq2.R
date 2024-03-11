80da3830c978359ff48401a37586eade5643f429
dakota-hawkins <dyh0110@bu.edu>
2024-03-10 21:23:22 -0400

    output formatting for .csv writing

--- scripts/deseq2.R
+++ scripts/deseq2.R
@@ -8,6 +8,8 @@ suppressMessages(library(pheatmap))
 suppressMessages(library(RColorBrewer))
 suppressMessages(library(gridExtra))
 library(grid)
+library(dplyr)
+library(RColorBrewer)
 
 theme_black = function(base_size = 12, base_family = "") {
   
@@ -54,16 +56,104 @@ theme_black = function(base_size = 12, base_family = "") {
   
 }
 
-treatment_de <- function(counts, coldata, treatment='Treatment',
-                         control='Control', batch='Batch') {
-    # set control value as 
-    levels = c(control, setdiff(unique(coldata[, treatment]),
-                                c(control)))
-    coldata[, treatment] <- factor(coldata[, treatment],
-                                   levels=levels)
+
+volcano_plot <- function(res, fc_threshold=1, p_threshold=0.05,
+                         title="", dark=TRUE) {
+    
+    res <- as.data.frame(res) %>%
+        dplyr::mutate(
+            Significant = dplyr::case_when(
+                !is.na(padj) & padj < p_threshold & log2FoldChange > fc_threshold ~ "+",
+                !is.na(padj) & padj < p_threshold & log2FoldChange < -fc_threshold ~ '-',
+                TRUE ~ "n.s."
+            ),
+            log10p = -log10(pvalue),
+            Significant = factor(Significant, levels=c("+", "-", "n.s."))
+        )
+
+    y_height <- min(res$log10p[res$Significant], na.rm=TRUE)
+    xticks1 <- seq(0, max(ceiling(res$log2FoldChange), na.rm=TRUE), 2)
+    xticks2 <- seq(0, min(floor(res$log2FoldChange), na.rm=TRUE), -2)
+    xticks <- sort(c(-fc_threshold, fc_threshold, xticks1, xticks2))
+    # yticks <- sort(c(seq(0, 1, 0.25), p_threshold))
+    p <- ggplot(data=res, aes(x=log2FoldChange, y=log10p, color=Significant)) +
+            geom_point(alpha=0.75) +
+            geom_vline(xintercept=fc_threshold, linetype='dotted') +
+            geom_vline(xintercept=-fc_threshold, linetype='dotted') +
+            geom_hline(yintercept=y_height, linetype='dotted') +
+            xlab("Log2FC") +
+            ylab('-Log10(p)') +
+            scale_x_continuous(breaks=xticks) +
+            ggtitle(title) + 
+            scale_color_manual(
+                values = c("+" = "#EF8A62", "-" = "#67A9CF", "n.s." = "#D3D3D3")
+            ) +
+            theme(legend.position = 'none',
+                  axis.text=element_text(size=14, face='bold'),
+                  axis.title=element_text(size=18, face="bold"))
+    if (dark) {
+        p <- p +
+            theme_black(22) +
+            scale_color_manual(values=c('#ff6ec7', "green", 'gray')) 
+            theme(legend.position = 'none',
+                  axis.text=element_text(size=14, face='bold'),
+                  axis.title=element_text(size=18, face="bold"))
+    }
+    return(p)
+}
+
+
+get_vsd_and_plot <- function(dds, plotdir, group) {
+    vsd <- vst(dds, blind=FALSE)
+    print("Plotting Principle Coordinates")
+    plotPCA(vsd, intgroup=group)
+    ggsave(
+        file.path(plotdir, 'treatment-sample-pca.png'),
+        width=9, height=7, dpi='retina'
+    )
+
+    # # plot dissimilarity matrix between samples
+    # print("Calculating correlations")
+    # sample_corr <- 1 - cor(assay(vsd))
+    # # sample_corr_mat <- as.matrix(sample_corr)
+    # rownames(sample_corr) <- colData(vsd)[, group]
+    # colnames(sample_corr) <- colData(vsd)[, group]
+    # colors <- colorRampPalette( rev(brewer.pal(9, "Blues")) )(255)
+
+    # p <- pheatmap(
+    #     sample_corr,
+    #     col=colors,
+    #     annotation_col=as.data.frame(colData(vsd))[c('HPF', 'Treatment', group)]
+    # )
+    # save_pheatmap_png <- function(x, filename, width=1200, height=1000, res = 150) {
+    #     png(filename, width = width, height = height, res = res)
+    #     grid::grid.newpage()
+    #     # grid.draw(rectGrob(gp=gpar(fill="black", lwd=0)))
+    #     grid::grid.draw(x$gtable)
+    #     # grid.gedit("layout", gp = gpar(col = "white", text = ""))
+    #     dev.off()
+    # }
+    # png(
+    #     file.path(plotdir, "sample-diss-matrix.png"),
+    #     width=1200,
+    #     height=1000,
+    #     res=150
+    # )
+    # dev.off()
+    # save_pheatmap_png(p, file.path(plotdir, 'sample-diss-matrix.png'))
+    warnings()
+    return(assay(vsd))
+}
+
+
+treatment_de <- function(counts, coldata, comparison_col, reference, experimental, name, plotdir, batch='Batch') {
+    if (!dir.exists(plotdir)) {
+        dir.create(plotdir)
+    }
+
     # compare between treatments
-    formula_str <- paste0('~ ', treatment)
-    if (!is.null(batch) || batch != '') {
+    formula_str <- paste0('~ ', comparison_col)
+    if (!is.null(batch)) {
         formula_str <- paste0(formula_str, ' + ', batch)
     }
     print(paste0('Formula: ', formula_str))
@@ -72,123 +162,114 @@ treatment_de <- function(counts, coldata, treatment='Treatment',
                                   colData=coldata,
                                   design=design)
     dds <- DESeq(dds)
-    res <- results(dds)
-    # plot DE genes
-    p <- volcano_plot(data.frame(res), dark=FALSE)
-    return(list('res'=data.frame(res), 'plot'=p))
-}
+    print("PLATDOAIDFAD")
+    print(plotdir)
+    deseq_results <- lapply(experimental, function(condition) {
+        condition_results <- results(dds, contrast=c(comparison_col, condition, reference))
+        condition_results$Comparision <- paste0(reference, "_vs_", condition)
 
-volcano_plot <- function(res, fc_threshold=1, p_threshold=0.05,
-                         title="", dark=TRUE) {
-    ggplot(data=res, aes(x='log2FoldChange', y='pvalue'))
-    res$log10p <- -log10(res$pvalue)
-    res$significant <- apply(res, 1, function(x) {
-        if (is.na(x['padj']) || is.na(x['log2FoldChange'])) {
-            return(FALSE)
-        }
-        if (x['padj'] <= p_threshold && abs(x['log2FoldChange']) >= fc_threshold) {
-            return(TRUE)
-        }
-        return(FALSE)
+        condition_results <- as.data.frame(condition_results) %>%
+            dplyr::mutate(
+                Significant = dplyr::case_when(
+                    !is.na(padj) & padj < p_threshold & log2FoldChange > fc_threshold ~ "+",
+                    !is.na(padj) & padj < p_threshold & log2FoldChange < -fc_threshold ~ '-',
+                    TRUE ~ "n.s."
+                ),
+                log10p = -log10(pvalue),
+                Significant = factor(Significant, levels=c("+", "-", "n.s."))
+            )
+
+        p <- volcano_plot(condition_results, dark=FALSE) +
+            ggtitle(paste0(reference, "_vs_", condition))
+        ggsave(
+            file.path(plotdir, paste0(reference, '_vs_', condition, '.png')),
+            plot = p,
+            width=9, height=7, dpi='retina'
+        )
+        
+        return(list(condition_results))
     })
-    y_height <- min(res$log10p[res$significant], na.rm=TRUE)
-    xticks1 <- seq(0, max(ceiling(res$log2FoldChange), na.rm=TRUE), 2)
-    xticks2 <- seq(0, min(floor(res$log2FoldChange), na.rm=TRUE), -2)
-    xticks <- sort(c(-fc_threshold, fc_threshold, xticks1, xticks2))
-    # yticks <- sort(c(seq(0, 1, 0.25), p_threshold))
-    p <- ggplot(data=res, aes(x=log2FoldChange, y=log10p, col=significant)) +
-            geom_point(alpha=0.75) +
-            geom_vline(xintercept=fc_threshold, linetype='dotted') +
-            geom_vline(xintercept=-fc_threshold, linetype='dotted') +
-            geom_hline(yintercept=y_height, linetype='dotted') +
-            xlab('Log2(FoldChange)') +
-            ylab('-Log10(p)') +
-            # scale_color_manual(values=c('black', 'red')) +
-            scale_x_continuous(breaks=xticks) +
-            ggtitle(title) + 
-            theme(legend.position = 'none',
-                  axis.text=element_text(size=14, face='bold'),
-                  axis.title=element_text(size=18, face="bold"))
-    if (dark) {
-        p <- p +
-            theme_black(22) +
-            scale_color_manual(values=c('gray', '#ff6ec7')) 
-            theme(legend.position = 'none',
-                  axis.text=element_text(size=14, face='bold'),
-                  axis.title=element_text(size=18, face="bold"))
-    } else {
-        p <- p + scale_color_manual(values=c('black', 'red')) 
-    }
-    return(p)
+    vsd <- get_vsd_and_plot(
+        dds,
+        plotdir,
+        comparison_col
+    )
+    return(list('normalized' = vsd, 'results'=do.call(rbind, deseq_results)))
 }
 
-main <- function(counts, coldata, comparisons, col, outdir,
-                 plotdir, treatment='Treatment', control='Control',
-                 batch='Batch') {
-    for (idx in seq(1, length(comparisons), 2)) {
-        each <- comparisons[idx:(idx + 1)]
-        print(paste0("Comparison: ", paste(each, collapse=' vs ')))
-        samples <- row.names(coldata)[apply(coldata, 1, function(x) x[col] %in% each)]
+main <- function(counts, coldata, comparisons, outdir,
+                 plotdir,
+                 batch=NULL) {
+    out_list <- list()
+    for (each in names(comparisons)) {
+        print(each)
+        # get comparison info
+        column <- comparisons[[each]][["column"]]
+        reference <- comparisons[[each]][["reference"]]
+        experimental <- comparisons[[each]][["experimental"]]
+        print(column)
+        for (every in experimental) {
+            print(every)
+            if (!every %in% coldata[, column]) {
+                stop(sprintf("%s not found in column %s", every, column))
+            }
+        }
+        # subset data to samples of interest
+        to_test <- c(reference, experimental)
+        print(paste0("Comparison: ", each))
+        samples <- row.names(coldata)[apply(coldata, 1, function(x) x[column] %in% to_test)]
         print(paste0("Samples ", paste(samples, collapse=', ')))
         sub_counts <- counts[ , samples]
         sub_coldata <- coldata[samples, ]
-        out <- treatment_de(sub_counts, sub_coldata, treatment, control, batch)
-        write.csv(out$res, file.path(outdir,
-                                     paste0(paste(each, collapse='_vs_'), '.csv')))
-        out$plot + ggtitle(paste(each, collapse=' vs '))
-        ggsave(file.path(plotdir, paste0(paste(each, collapse='_vs_'), '.png')),
-               width=9, height=7, dpi='retina')
+        # set reference values
+        print("ORIGINAL")
+        print(plotdir)
+        out <- treatment_de(
+            sub_counts,
+            sub_coldata,
+            column,
+            reference,
+            experimental,
+            each,
+            plotdir=file.path(plotdir, each, "plots", "deseq2"),
+            batch
+        )
+        print("WRITING HERE:            !!!!!")
+        print(outdir)
+        write.csv(
+            out$results,
+            file.path(outdir, each, "deseq2", "results.csv")
+        )
+        write.csv(
+            out$normalized,
+            file.path(outdir, each, "deseq2", "normalized_counts.csv")
+        )
+        print("wrote all output")       
     }
 }
 
+
 if (exists("snakemake")) {
     coldata <- read.csv(snakemake@input$samples, row.names=1, check.names=FALSE)
+    if (!length(intersect(c("Sample", "HPF", "Treatment"), colnames(coldata)))) {
+        stop("Expected Sample and HPF columns. This is case sensitive.")
+    }
     coldata$HPF <- as.factor(coldata$HPF)
     # load counts and match sample order
     counts <- read.csv(snakemake@input$counts,
                        row.names=1,
-                       check.names=FALSE)[ , row.names(coldata)]
+                       check.names=FALSE)
+    # not ideal, fix later or not
+    colnames(counts) <- gsub("^sample-", "", colnames(counts))
+    counts <- counts[ , row.names(coldata)]
     # run deseq2 on desired comparisons
     print(snakemake@params$comparisons)
-    main(counts, coldata, snakemake@params$comparisons, snakemake@params$col,
-         snakemake@params$outdir, snakemake@params$plotdir,
-         snakemake@params$treatment, snakemake@params$control,
-         snakemake@params$batch)
-
-    # plot pca of all samples
-    design = as.formula(paste0('~ ', snakemake@params$treatment))
-    dds <- DESeqDataSetFromMatrix(countData=counts,
-                                  colData=coldata,
-                                  design=design)
-    vsd <- vst(dds, blind=FALSE)
-    print("Plotting Principle Coordinates")
-    plotPCA(vsd, intgroup=c("Treatment", "HPF")) + theme_black(14)
-    ggsave(file.path(snakemake@params$plotdir, 'treatment-sample-pca.png'),
-           width=9, height=7, dpi='retina')
-
-
-    # plot dissimilarity matrix between samples
-    print("Calculating correlations")
-    sample_corr <- 1 - cor(assay(vsd))
-    # sample_corr_mat <- as.matrix(sample_corr)
-    rownames(sample_corr) <- paste(vsd$Group, vsd$Sample, sep="-")
-    colnames(sample_corr) <- paste(vsd$Group, vsd$Sample, sep="-")
-    row.names(coldata) <- row.names(sample_corr)
-    colors <- colorRampPalette( rev(brewer.pal(9, "Blues")) )(255)
-
-    p <- pheatmap(sample_corr,
-                  col=colors,
-                  annotation_col=coldata[c('HPF', 'Treatment', 'Sample')])
-    save_pheatmap_png <- function(x, filename, width=1200, height=1000, res = 150) {
-        png(filename, width = width, height = height, res = res)
-        grid::grid.newpage()
-        grid.draw(rectGrob(gp=gpar(fill="black", lwd=0)))
-        grid::grid.draw(x$gtable)
-        grid.gedit("layout", gp = gpar(col = "white", text = ""))
-        dev.off()
-    }
-
-    save_pheatmap_png(p, file.path(snakemake@params$plotdir, 'sample-diss-matrix.png'))
-    warnings()
-    write.csv(assay(vsd), snakemake@output$counts)
+    main(
+        counts,
+        coldata,
+        snakemake@params$comparisons,
+        snakemake@params$outdir,
+        snakemake@params$plotdir,
+        snakemake@params$batch
+    )    
 }
